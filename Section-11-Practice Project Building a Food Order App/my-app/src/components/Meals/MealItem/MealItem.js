a7ad5a6a5b50f8d7726829d9669ec2319c96c393
DESKTOP-E141O5H\Vijju <imvijju94negi@gmail.com>
2022-09-05 10:34:34 +0530

    Adding Individual Meal Items & Displaying Them

---
+++ Section-11-Practice Project Building a Food Order App/my-app/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,16 @@
+import classes from "../../Css/MealItem.module.css";
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h2>{props.name}</h2>
+      </div>
+      <div className={classes.description}>{props.description}</div>
+      <div className={classes.price}>{price}</div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
---
+++ Section-11-Practice Project Building a Food Order App/my-app/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./Available";
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
