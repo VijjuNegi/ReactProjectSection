a9f893ce6dc674e5103b1dd24c662c183c04c912
DESKTOP-E141O5H\Vijju <imvijju94negi@gmail.com>
2022-09-06 14:49:12 +0530

    Working with Refs & Forward Refs

--- Section-11-Practice Project Building a Food Order App/my-app/src/components/Meals/MealItem/MealItem.js
+++ Section-11-Practice Project Building a Food Order App/my-app/src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,20 @@
+import { useContext } from "react";
 import MealItemForm from "./MealItemForm";
 import classes from "../../Css/MealItem.module.css";
+import CardContext from "../../../store/card-context";
 const MealItem = (props) => {
+  const cardCtx = useContext(CardContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCardHandler = (amount) => {
+    cardCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -10,7 +23,7 @@ const MealItem = (props) => {
       <div className={classes.description}>{props.description}</div>
       <div className={classes.price}>{price}</div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCard={addToCardHandler} />
       </div>
     </li>
   );
